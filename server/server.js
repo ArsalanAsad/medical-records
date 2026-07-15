7f6a98496521ee7311ec8264b55265e330d3fa8f
Arsalan Asad <arsalan.gazdar@gmail.com>
2026-07-15 15:34:33 +0500

    Fix Vercel SPAII routing

--- server/server.js
+++ server/server.js
@@ -11,24 +11,35 @@ const app = express();
 
 connectDB();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:3000",
-  process.env.CLIENT_URL,
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://localhost:3000",
+//   process.env.CLIENT_URL,
+// ];
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       // Postman ya server-to-server requests ko allow kare
+//       if (!origin) return callback(null, true);
+
+//       if (allowedOrigins.includes(origin)) {
+//         return callback(null, true);
+//       }
+
+//       return callback(new Error("Not allowed by CORS"));
+//     },
+//     credentials: true,
+//   })
+// );
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // Postman ya server-to-server requests ko allow kare
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://medical-records-nn26.vercel.app",
+    ],
     credentials: true,
   })
 );
